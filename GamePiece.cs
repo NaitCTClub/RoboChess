bcb7d71f108cd1097e7d1a7d40d3ac776832cb17
NaitCTClub <43456340+NaitCTClub@users.noreply.github.com>
2018-09-27 17:33:07 -0600

    bully update

---
+++ GamePiece.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Chess
+{
+    /**
+     * @brief The class that all game pieces are based off of.
+     **/
+    public abstract class GamePiece
+    {
+        public Color PieceColor { get; protected set; } //The color of the gamie piece.
+        public int ID { get; protected set; }  //Identifier for the piece
+        public bool isAlive { get; protected set; } //Indicates if the piece is still active on the board.
+
+        //Constructor for the game piece object. Initialize all parameters.
+        protected GamePiece(Color pieceColor, int id)
+        {
+            //Set the internal members from the passed in values.
+            PieceColor = pieceColor;
+            ID = id;
+            //All pieces start out as active.
+            isAlive = true;
+
+
+        }
+
+        public abstract void Move();
+    }
+
+
+}
