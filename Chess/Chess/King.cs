ef4d2d01829cc54a8431d27a97621d69763775df
Ben Gullason <bendgullason@gmail.com>
2018-09-27 17:20:58 -0600

    Made King class

---
+++ Chess/Chess/King.cs
@@ -0,0 +1,21 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Chess
+{
+    /**
+    * @brief The pawn chess piece.
+    **/
+    public class Bishop : GamePiece
+    {
+        /**
+         * @brief Constructor for the Pawn object. Calls the constructor for the base GamePiece
+         * class before it does it's own construction.
+         **/
+        Bishop(Color pieceColor, int id) : base(pieceColor, id) { }
+    }
+}
