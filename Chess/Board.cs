8b868b112bbbe54be3881d326e22fed9361afc79
Jonathon Klassen <35517036+jklassen@users.noreply.github.com>
2018-10-04 17:23:03 -0600

    ddfx

---
+++ Chess/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Chess
+{
+    class Board
+    {
+        //GamePiece[,] cells = new GamePiece[8, 8];
+
+        
+
+
+    }
+}
